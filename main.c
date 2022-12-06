732c0b59633dad69170eefd9db1d5ea045a5afd9
shinjuhyun <112927859+shinjuhyun@users.noreply.github.com>
2022-12-06 16:35:29 +0900

    02

--- main.c
+++ main.c
@@ -2,21 +2,27 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct Book{
+	int number;
+	char title[10];
+};
 
-int main(int argc, char *argv[]) {
-	char *pc = NULL;
-	int i = 0;
+int main(void) {
+	struct Book *p;
 	
-	pc = (char*)malloc(100*sizeof(char));
-	if(pc==NULL){
+	p = (struct Book*)malloc(2*sizeof(struct Book));
+	
+	if(p == NULL){
 		printf("error\n");
-		return (-1);
+		return -1;
 	}
-	for(i=0;i<26;i++){
-		pc[i] = 'a'+1;
-	}
-	pc[i] = 0;
-	printf("%s\n",pc);
-	free(pc);
+	p->number = 1;
+	strcpy(p->title, "C Programming");
+	
+	(p+1)->number = 2;
+	strcpy((p+1)->title,"Electronics");
+	
+	free(p);
+	
 	return 0;
 }
